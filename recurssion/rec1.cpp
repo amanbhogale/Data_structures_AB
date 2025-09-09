874656cc4592af1be2504f9d1642468080cdc09f
amanbhogale <amanbhogale0@gmail.com>
2025-09-09 22:53:17 +0530

    recurssion_file_edited

--- recurssion/rec1.cpp
+++ recurssion/rec1.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void func1(int n){
+/*void func1(int n){
 	if(n > 0){
 		cout << n << " ";
 		func1(n-1);
 	}
-}
+}*/
+
+void func2(int n){
+	if(n >0){
+		func2(n-1);
+		cout << n << " ";
+	}
+} 
 
 int main(){
 	int n = 10;
-	func1(n);
+	func2(n);
 	return 0;
 
 }
